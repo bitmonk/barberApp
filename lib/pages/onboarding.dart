56a9478e04550b9b3a88a6a263c7bab1fdf55b00
Parbat Tamang <parbattamang1234@gmail.com>
2024-05-17 11:40:55 +0545

    backup

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -11,8 +11,14 @@ class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Column(children: [
+      backgroundColor: Color(0xFF2b1615),
+      body: Container(
+        margin: EdgeInsets.only(top:120.0),
+        child: Column(children: [
         Image.asset("images/barber.png"),
+        Container(
+          child: Text("Get a Stylish Haircut", style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold)),
+        )
       ],),),
     );
   }
